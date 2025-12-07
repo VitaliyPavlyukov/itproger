c15eb50698b8e6a6a44e3b5359b7102b8da4eb39
Виталий Павлюков <aikimind@mail.ru>
2025-12-07 13:20:18 +0300

    movies

--- src/app/pages/movies/movies.ts
+++ src/app/pages/movies/movies.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { MovieService } from '../../services/movies';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-movies',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './movies.html',
   styleUrl: './movies.css',
 })
 export class Movies {
+  constructor(private movieService: MovieService) {}
 
+    searchTitle: string = '';
+    movies: any[] = [];
+    errorMessage: string = '';
+
+    search() {
+      if(!this.searchTitle.trim()) return;
+
+      this.movieService.searchMovies(this.searchTitle).subscribe({
+        next: (response) => {
+          if (response.Search) {
+            this.movies = response.Search;
+            this.errorMessage = '';
+          } else {
+            this.movies = [];
+            this.errorMessage = response.Error || 'No results';
+          }
+        },
+        error: () => {
+          this.errorMessage = 'Error fetching movies';
+          this.movies = [];
+        }
+      });
+    }  
 }
--- src/app/services/movies.ts
+++ src/app/services/movies.ts
@@ -5,7 +5,15 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class Movies {
+
+export class MovieService {
   private apiUrl = 'https://omdbapi.com/';
   private apiKey = '13cfd603';
+
+  constructor(private http: HttpClient) {}
+    
+  searchMovies(title: string): Observable<any> {
+      return this.http.get<any>(`${this.apiUrl}?s=${title}&apikey=${this.apiKey}`);
+    }
+
 }
